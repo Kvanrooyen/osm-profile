78e02f726910499690168888ee081296f4f47f90
Keagan <github@governorkeagan.com>
2024-06-11 14:27:17 +0100

    feat: added placeholder for recent changesets

--- src/components/RecentChangesets.js
+++ src/components/RecentChangesets.js
@@ -7,6 +7,7 @@ import './css/RecentChangesets.css';
 
 const RecentChangesets = () => {
   const [recentChangesets, setRecentChangesets] = useState([]);
+  const [loading, setLoading] = useState(true); // Add loading state
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -21,9 +22,11 @@ const RecentChangesets = () => {
           .slice(0, 5);
 
         setRecentChangesets(sortedChangesets);
+        setLoading(false); // Set loading to false once data is fetched
       } catch (error) {
         console.error('Error fetching recent changesets:', error); // Add logging
         setError(error.message);
+        setLoading(false); // Set loading to false in case of error
       }
     };
 
@@ -34,21 +37,32 @@ const RecentChangesets = () => {
     <div className="profile-overview recent-changesets">
       <h2>Recent Changesets</h2>
       {error && <p>Error: {error}</p>}
-      <ul>
-        {recentChangesets.map((changeset, index) => (
-          <li key={index} className="changeset-item">
-            <a 
-              className="changeset-link"
-              href={`https://www.openstreetmap.org/changeset/${changeset.changesetId}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div className="changeset-comment">{changeset.comment}</div>
-              <div className="changeset-time">{formatDistanceToNow(new Date(changeset.createdAt))} ago</div>
-            </a>
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <ul>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <li key={index} className="changeset-placeholder">
+              <div className="placeholder-comment"></div>
+              <div className="placeholder-time"></div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <ul>
+          {recentChangesets.map((changeset, index) => (
+            <li key={index} className="changeset-item">
+              <a
+                className="changeset-link"
+                href={`https://www.openstreetmap.org/changeset/${changeset.changesetId}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="changeset-comment">{changeset.comment}</div>
+                <div className="changeset-time">{formatDistanceToNow(new Date(changeset.createdAt))} ago</div>
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
